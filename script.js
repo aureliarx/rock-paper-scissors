3ea19bcf494d21442f2461f96e02087124677c5d
Aurelia <aureliarnj@gmail.com>
2023-11-15 18:16:09 +0700

    Update: added a reload button to restart the game that has ended

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let playerScore = 0;
 let computerScore = 0;
+let winnerRound = '';
+let result = '';
 const buttons = document.querySelectorAll('input');
+const restart = document.getElementById('restart');
+restart.hidden = true;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => playRound(button.value));
+});
 
 function disableButtons() {
     buttons.forEach(b => {
         b.disabled = true;
     });
 };
+
+function reloadGame() {
+    window.location.reload('restart');
+}
 
 function getRandomChoice() {
     const rps = ['rock', 'paper', 'scissors'];
@@ -15,36 +27,44 @@ function getRandomChoice() {
 
 function playRound(playerSelection) {
     let computerSelection = getRandomChoice();
-    let result = '';
 
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'paper') || 
         (playerSelection == 'paper' && computerSelection == 'rock')) {
+
         playerScore += 1;
+        winnerRound = 'player';
+        
         result = (`You win! ${playerSelection} beats ${computerSelection}. <br><br>Player score: ${playerScore} <br>Computer score: ${computerScore}`);
 
-        if (playerScore == 5) {
-            result += '<br><br>You won the game! Reload the page to restart the game.'
-            disableButtons();
-        }
+        gameOver(playerScore);
     } else if (playerSelection == computerSelection) {
         result = (`It's tie. You both choose ${playerSelection}. <br><br>Player turn: ${playerScore} <br>Computer score: ${computerScore}`);
     } else {
         computerScore += 1;
+        winnerRound = 'computer';
+
         result = (`You lose! ${computerSelection} beats ${playerSelection}. <br><br>Player score: ${playerScore} <br>Computer score: ${computerScore}`);
 
-        if (computerScore == 5) {
-            result += `<br><br>I won the game! Reload the page to restart the game.`
-            disableButtons();
-        }
+        gameOver(computerScore);
     }
 
     document.getElementById('result').innerHTML = result;
-    return
+    return result;
 };
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        playRound(button.value);
-    });
-});
+function gameOver(score) {
+    if (score == 5) {
+        disableButtons();
+        if (winnerRound == 'player') {
+            result += '<br><br>Yay! You won the game!'
+        } else {
+            result += '<br><br>Haha! I won the game!'
+        }
+
+        restart.hidden = false;
+        restart.addEventListener('click', reloadGame);
+    }
+    document.getElementById('result').innerHTML = result;
+    return result;
+};
